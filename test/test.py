6e8974e894a60eb2995e920cd14270951b82cb0f
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-27 16:53:07 -0600

    updates

--- test/test.py
+++ test/test.py
@@ -1,10 +1,12 @@
 import ncepbufr
 hdstr='SID XOB YOB DHR TYP ELV SAID T29'
-obstr='POB QOB TOB ZOB UOB VOB PWO CAT PRSS'
-qcstr='PQM QQM TQM ZQM WQM NUL PWQ     '
+#hdstr2='TYP SAID T29 SID'
+obstr='POB QOB TOB ZOB UOB VOB PWO MXGS HOVI CAT PRSS TDO PMO'
+qcstr='PQM QQM TQM ZQM WQM NUL PWQ PMQ'
 oestr='POE QOE TOE NUL WOE NUL PWE     '
-mxmn = 35; mxlv = 250
+mxlv = 255
 lunit = 7
+missval = 1.e11
 # open bufr file
 ncepbufr.openbf('prepbufr',lunit,'IN',lunit)
 # set date length
@@ -18,15 +20,15 @@ else:
     raise ValueError('nonzero return code = %s from readmg' % iret)
 iret = ncepbufr.ireadsb(lunit)
 if iret == 0:
-    hdr,ilevs = ncepbufr.ufbint(lunit,mxmn,1,hdstr)
+    hdr,ilevs = ncepbufr.ufbint(lunit,len(hdstr.split()),1,hdstr)
     station_id = hdr[0].tostring()
     print 'station_id, lon, lat, time, station_type =',\
     station_id,hdr[1].item(),hdr[2].item(),hdr[3].item(),int(hdr[4].item())
-    obs, ilevs = ncepbufr.ufbint(lunit,mxmn,mxlv,obstr)
-    oer, ilevs = ncepbufr.ufbint(lunit,mxmn,mxlv,oestr)
-    qcf, ilevs = ncepbufr.ufbint(lunit,mxmn,mxlv,qcstr)
+    obs, ilevs = ncepbufr.ufbint(lunit,len(obstr.split()),mxlv,obstr)
+    oer, ilevs = ncepbufr.ufbint(lunit,len(oestr.split()),mxlv,oestr)
+    qcf, ilevs = ncepbufr.ufbint(lunit,len(qcstr.split()),mxlv,qcstr)
     for k in xrange(ilevs):
         print 'level',k+1
-        print 'obs',obs[0:9,k]
-        print 'oer',oer[0:7,k]
-        print 'qcf',qcf[0:7,k]
+        print 'obs',obs[:,k]
+        print 'oer',oer[:,k]
+        print 'qcf',qcf[:,k]
