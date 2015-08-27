0cef572f19949e4ff37ac26743f9d7f47296a340
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-27 14:45:13 -0600

    fix signature for readmg, ireadmg

--- test/test.py
+++ test/test.py
@@ -1,4 +1,12 @@
 import ncepbufr
 lunit = 7
+# open bufr file
 ncepbufr.openbf('prepbufr',lunit,'IN',lunit)
+# set date length
+ncepbufr.datelen(10)
+# dump bufr table
 ncepbufr.dxdump('prepbufr.table',lunit,lunit+1)
+subset, idate, iret = ncepbufr.readmg(lunit)
+print subset
+print idate
+print iret
