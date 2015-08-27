f2b8944ec215e053d0776116a779924b12cb25fb
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-27 17:02:31 -0600

    loop over all messages

--- test/test.py
+++ test/test.py
@@ -13,22 +13,24 @@ ncepbufr.openbf('prepbufr',lunit,'IN',lunit)
 ncepbufr.datelen(10)
 # dump bufr table
 ncepbufr.dxdump('prepbufr.table',lunit,lunit+1)
-subset, idate, iret = ncepbufr.readmg(lunit)
-if iret == 0:
-    print 'subset, date =',subset, idate
-else:
-    raise ValueError('nonzero return code = %s from readmg' % iret)
-iret = ncepbufr.ireadsb(lunit)
-if iret == 0:
-    hdr,ilevs = ncepbufr.ufbint(lunit,len(hdstr.split()),1,hdstr)
-    station_id = hdr[0].tostring()
-    print 'station_id, lon, lat, time, station_type =',\
-    station_id,hdr[1].item(),hdr[2].item(),hdr[3].item(),int(hdr[4].item())
-    obs, ilevs = ncepbufr.ufbint(lunit,len(obstr.split()),mxlv,obstr)
-    oer, ilevs = ncepbufr.ufbint(lunit,len(oestr.split()),mxlv,oestr)
-    qcf, ilevs = ncepbufr.ufbint(lunit,len(qcstr.split()),mxlv,qcstr)
-    for k in xrange(ilevs):
-        print 'level',k+1
-        print 'obs',obs[:,k]
-        print 'oer',oer[:,k]
-        print 'qcf',qcf[:,k]
+# loop over messages
+nmsg = 0
+while 1:
+    subset, idate, iret = ncepbufr.readmg(lunit)
+    if iret != 0: break
+    nmsg += 1
+    print 'nmsg, subset, date =',nmsg, subset, idate
+    # loop reports in message
+    while (ncepbufr.ireadsb(lunit) == 0):
+        hdr,ilevs = ncepbufr.ufbint(lunit,len(hdstr.split()),1,hdstr)
+        station_id = hdr[0].tostring()
+        obs, ilevs = ncepbufr.ufbint(lunit,len(obstr.split()),mxlv,obstr)
+        oer, ilevs = ncepbufr.ufbint(lunit,len(oestr.split()),mxlv,oestr)
+        qcf, ilevs = ncepbufr.ufbint(lunit,len(qcstr.split()),mxlv,qcstr)
+        print 'station_id, lon, lat, time, station_type, levels =',\
+        station_id,hdr[1].item(),hdr[2].item(),hdr[3].item(),int(hdr[4].item()),ilevs
+        #for k in xrange(ilevs):
+        #    print 'level',k+1
+        #    print 'obs',obs[:,k]
+        #    print 'oer',oer[:,k]
+        #    print 'qcf',qcf[:,k]
