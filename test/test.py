02c9531419dadf351e1454e414f6bc2621232ce1
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-27 15:27:55 -0600

    fix interface for ufbint

--- test/test.py
+++ test/test.py
@@ -1,4 +1,10 @@
 import ncepbufr
+import struct
+hdstr='SID XOB YOB DHR TYP ELV SAID T29'
+obstr='POB QOB TOB ZOB UOB VOB PWO CAT PRSS'
+qcstr='PQM QQM TQM ZQM WQM NUL PWQ     '
+oestr='POE QOE TOE NUL WOE NUL PWE     '
+mxmn = 35; mxlv = 250
 lunit = 7
 # open bufr file
 ncepbufr.openbf('prepbufr',lunit,'IN',lunit)
@@ -7,6 +13,12 @@ ncepbufr.datelen(10)
 # dump bufr table
 ncepbufr.dxdump('prepbufr.table',lunit,lunit+1)
 subset, idate, iret = ncepbufr.readmg(lunit)
-print subset
-print idate
-print iret
+if iret == 0:
+    print 'subset, date =',subset, idate
+else:
+    raise ValueError('nonzero return code = %s from readmg' % iret)
+iret = ncepbufr.ireadsb(lunit)
+if iret == 0:
+    hdr,iret = ncepbufr.ufbint(lunit,mxmn,1,hdstr)
+    station_id = hdr[0].tostring()
+    print 'station id =',station_id
