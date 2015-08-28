b85d82e28d9f1281229b6d5c79a36b38b2e0ddf2
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-28 11:45:53 -0600

    update

--- ncepbufr/__init__.py
+++ ncepbufr/__init__.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     print _funits
 
     # read radiance file.
-    hdstr1 ='SAID FOVN YEAR MNTH DAYS HOUR MINU SECO CLAT CLON CLATH CLONH HOLS'
+    hdstr1 ='SAID SIID FOVN YEAR MNTH DAYS HOUR MINU SECO CLAT CLON CLATH CLONH HOLS'
     hdstr2 ='SAZA SOZA BEARAZ SOLAZI'
     bufr = open('../test/1bamua')
     bufr.print_table()
@@ -155,9 +155,13 @@ if __name__ == "__main__":
         while (bufr.load_subset() == 0):
             hdr1 = bufr.read_subset(hdstr1)
             hdr2 = bufr.read_subset(hdstr2)
-            yyyymmddhhss ='%04i%02i%02i%02i%02i%02i' % tuple(hdr1[2:8])
-            print 'satid, lat, lon, yyyymmddhhmmss =',int(hdr1[0].item()),\
-            hdr1[8].item(),hdr1[9].item(),yyyymmddhhss
+            yyyymmddhhss ='%04i%02i%02i%02i%02i%02i' % tuple(hdr1[3:9])
+            # for satellite id, see common code table c-5
+            # (http://www.emc.ncep.noaa.gov/mmb/data_processing/common_tbl_c1-c5.htm#c-5)
+            # for sensor id, see common code table c-8
+            # (http://www.emc.ncep.noaa.gov/mmb/data_processing/common_tbl_c8-c14.htm#c-8)
+            print 'sat id, sensor id, lat, lon, yyyymmddhhmmss =',int(hdr1[0].item()),\
+            int(hdr1[1].item()),hdr1[9].item(),hdr1[10].item(),yyyymmddhhss
             if print_data: # print data from first subset with data
                 obs = bufr.read_subset('TMBR',pivot=True)
                 nchanl = obs.shape[-1]
