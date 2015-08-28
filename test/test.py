07ab1e75980749454dd711a57143d3a3bf665099
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-28 11:32:25 -0600

    update

--- test/test.py
+++ test/test.py
@@ -43,8 +43,10 @@ for subset in bufr:
         hdr1 = bufr.read_subset(hdstr1)
         hdr2 = bufr.read_subset(hdstr2)
         yyyymmddhhss ='%04i%02i%02i%02i%02i%02i' % tuple(hdr1[3:9])
-        # for satid, see common code table c-5
+        # for satellite id, see common code table c-5
         # (http://www.emc.ncep.noaa.gov/mmb/data_processing/common_tbl_c1-c5.htm#c-5)
+        # for sensor id, see common code table c-8
+        # (http://www.emc.ncep.noaa.gov/mmb/data_processing/common_tbl_c8-c14.htm#c-8)
         print 'sat id,sensor id lat, lon, yyyymmddhhmmss =',int(hdr1[0].item()),\
         int(hdr1[1].item()),hdr1[9].item(),hdr1[10].item(),yyyymmddhhss
         if print_data: # print data from first subset with data
