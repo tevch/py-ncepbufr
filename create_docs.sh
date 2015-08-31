8ebd9daa6c31b20bce193e294f983757b8bce143
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-31 10:32:08 -0600

    update

--- create_docs.sh
+++ create_docs.sh
@@ -1,5 +1,3 @@
 # Uses pdoc (https://github.com/BurntSushi/pdoc)
-# to create html docs from docstrings in Cython source.
-# Use hacked version at https://github.com/jswhit/pdoc
-# which extracts cython method docstrings and function signatures.
+# to create html docs from docstrings in python source.
 pdoc --html --html-no-source --overwrite --html-dir 'docs' ncepbufr
