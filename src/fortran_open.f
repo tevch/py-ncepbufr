5f7b745e5abd9116ba9c5bd90b4745363841d898
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-31 17:01:25 -0600

    add ability to pass position kwarg to fortran open

--- src/fortran_open.f
+++ src/fortran_open.f
@@ -1,7 +1,8 @@
-      subroutine fortran_open(filename, lunit, format, iret)
-      character*(*), intent(in) :: filename, format
+      subroutine fortran_open(filename, lunit, format, position, iret)
+      character*(*), intent(in) :: filename, format, position
       integer,intent(in) :: lunit
       integer,intent(out) :: iret
-      open(lunit,file=trim(filename),form=trim(format),iostat=iret)
+      open(lunit,file=trim(filename),form=trim(format),
+     *     position=trim(position),iostat=iret)
       return
       end
