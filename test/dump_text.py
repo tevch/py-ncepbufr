6faa05e1a1b57c8a3b55b5880dbd7af3ba825bb4
Jeff Whitaker <jswhit@fastmail.fm>
2015-08-31 14:02:09 -0600

    update

--- test/dump_text.py
+++ test/dump_text.py
@@ -6,16 +6,20 @@ import sys
 # Warning: resulting output may be HUGE.
 
 bufr = ncepbufr.open(sys.argv[1])
-first_dump = True
+first_dump = True # after first write, append to existing file.
+verbose = False # this produces more readable output.
 while bufr.advance() == 0: # loop over messages.
     while bufr.load_subset() == 0: # loop over subsets in message.
         if len(sys.argv) > 2:
             # dump decoded data to a file.
             if first_dump:
-                bufr.dump_subset(sys.argv[2]) # print decoded subset to stdout
+                # clobber file if it exists
+                bufr.dump_subset(sys.argv[2],verbose=verbose)
                 first_dump = False
             else:
-                bufr.dump_subset(sys.argv[2],append=True) # print decoded subset to stdout
+                # append to existing file.
+                bufr.dump_subset(sys.argv[2],append=True,verbose=verbose)
         else:
-            bufr.print_subset() # print decoded subset to stdout
+            bufr.print_subset(verbose=verbose) # print decoded subset to stdout
+        raise SystemExit
 bufr.close()
