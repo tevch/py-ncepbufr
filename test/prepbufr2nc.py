8dba8ad3a93e7881fa7d0f174c81d4fbb660b058
Jeff Whitaker <jswhit@github.com>
2015-09-04 11:50:42 -0400

    updates

--- test/prepbufr2nc.py
+++ test/prepbufr2nc.py
@@ -1,70 +1,12 @@
 import ncepbufr
 import numpy as np
 from netCDF4 import Dataset
+from prepbufr_mnemonics import mnemonics_dict
 
 hdstr='SID XOB YOB DHR TYP ELV SAID T29'
-hdrstr_dict = {}
-hdrstr_dict['XOB'] = 'longitude (degrees)'
-hdrstr_dict['YOB'] = 'latitude (degrees)'
-hdrstr_dict['DHR'] = 'observation time minus cycle time (hours)'
-hdrstr_dict['TYP'] = 'prepbufr report type'
-hdrstr_dict['ELV'] =  'station elevation (m)'
-hdrstr_dict['T29'] = 'data dump report type'
-hdrstr_dict['SAID'] = 'satellite identifier'
 obstr='POB QOB TOB ZOB UOB VOB PWO MXGS HOVI CAT PRSS TDO PMO'
-obstr_dict = {}
-obstr_dict['POB'] = 'pressure observation (mb)'
-obstr_dict['QOB'] = 'specific humidity observation (mg/Kg)'
-obstr_dict['TOB'] = 'temperature observation (C)'
-obstr_dict['ZOB'] = 'height observation (m)'
-obstr_dict['UOB'] = 'zonal wind observation (m/s)'
-obstr_dict['VOB'] = 'meridional wind observation (m/s)'
-obstr_dict['PWO'] = 'precipitable water observation (mm)'
-obstr_dict['MXGS'] = 'maximum wind speed (gusts) (m/s)'
-obstr_dict['HOVI'] = 'horizontal visibility (m)'
-obstr_dict['CAT'] = 'prepbufr data level category'
-obstr_dict['PRSS'] = 'surface pressure observation (Pa)'
-obstr_dict['TDO'] = 'dewpoint temperature observation (C)'
-obstr_dict['PMO'] = 'mean sea-level observation (mb)'
 qcstr='PQM QQM TQM ZQM WQM NUL PWQ PMQ'
-qcstr_dict={}
-qcstr_dict['PQM'] = 'pressure quality marker'
-qcstr_dict['QQM'] = 'specific humidity quality marker'
-qcstr_dict['TQM'] = 'temperature quality marker'
-qcstr_dict['ZQM'] = 'height quality marker'
-qcstr_dict['WQM'] = 'wind quality marker'
-qcstr_dict['NUL'] = 'empty'
-qcstr_dict['PWQ'] = 'precipitable water quality marker'
-qcstr_dict['PMQ'] = 'mean sea-level quality marker'
 oestr='POE QOE TOE NUL WOE NUL PWE'
-oestr_dict={}
-oestr_dict['POE'] = 'pressure observation error'
-oestr_dict['QOE'] = 'specific humidity observation error'
-oestr_dict['TOE'] = 'temperature observation error'
-oestr_dict['WOE'] = 'wind observation error'
-oestr_dict['NUL'] = 'missing'
-oestr_dict['PWE'] = 'precipitable water observation error'
-grpdict = {}
-grpdict["ADPUPA"] = "UPPER-AIR (RAOB, PIBAL, RECCO, DROPS) REPORTS            "
-grpdict["AIRCAR"] = "MDCRS ACARS AIRCRAFT REPORTS                             "
-grpdict["AIRCFT"] = "AIREP, PIREP, AMDAR, TAMDAR AIRCRAFT REPORTS             "
-grpdict["SATWND"] = "SATELLITE-DERIVED WIND REPORTS                           "
-grpdict["PROFLR"] = "WIND PROFILER REPORTS                                    "
-grpdict["VADWND"] = "VAD (NEXRAD) WIND REPORTS                                "
-grpdict["SATEMP"] = "TOVS SATELLITE DATA (SOUNDINGS, RETRIEVALS, RADIANCES)   "
-grpdict["ADPSFC"] = "SURFACE LAND (SYNOPTIC, METAR) REPORTS                   "
-grpdict["SFCSHP"] = "SURFACE MARINE (SHIP, BUOY, C-MAN PLATFORM) REPORTS      "
-grpdict["SFCBOG"] = "MEAN SEA-LEVEL PRESSURE BOGUS REPORTS                    "
-grpdict["SPSSMI"] = "SSM/I RETRIEVAL PRODUCTS (REPROCESSED WIND SPEED, TPW)   "
-grpdict["SYNDAT"] = "SYNTHETIC TROPICAL CYCLONE BOGUS REPORTS                 "
-grpdict["ERS1DA"] = "ERS SCATTEROMETER DATA (REPROCESSED WIND SPEED)          "
-grpdict["GOESND"] = "GOES SATELLITE DATA (SOUNDINGS, RETRIEVALS, RADIANCES)   "
-grpdict["QKSWND"] = "QUIKSCAT SCATTEROMETER DATA (REPROCESSED)                "
-grpdict["MSONET"] = "MESONET SURFACE REPORTS (COOPERATIVE NETWORKS)           "
-grpdict["GPSIPW"] = "GLOBAL POSITIONING SATELLITE-INTEGRATED PRECIP. WATER    "
-grpdict["RASSDA"] = "RADIO ACOUSTIC SOUNDING SYSTEM (RASS) TEMP PROFILE RPTS  "
-grpdict["WDSATR"] = "WINDSAT SCATTEROMETER DATA (REPROCESSED)                 "
-grpdict["ASCATW"] = "ASCAT SCATTEROMETER DATA (REPROCESSED)                   "
 
 # read prepbufr file, write data to netcdf file.
 
@@ -79,12 +21,13 @@ bufr = ncepbufr.open('prepbufr')
 while bufr.advance() == 0: # loop over messages.
     g = nc.createGroup(bufr.msg_type)
     if not g.variables.has_key('obdata'):
-        g.setncattr('desc',grpdict[bufr.msg_type].rstrip())
+        g.setncattr('desc',mnemonics_dict[bufr.msg_type].rstrip())
         nobs = g.createDimension('nobs',None)
         hdrdata = g.createVariable('header',np.float32,('nobs','header'),zlib=True)
         stnid = g.createVariable('stationid',str,('nobs',))
-        for key in hdrstr_dict:
-            hdrdata.setncattr(key,hdrstr_dict[key])
+        stnid.info = 'STATION IDENTIFICATION'
+        for key in hdstr.split()[1:]:
+            hdrdata.setncattr(key,mnemonics_dict[key])
         hdrdata.missing_value = 1.e11
         hdrdata.info = hdstr[4:]
         if bufr.msg_type in ['RASSDA','VADWND','PROFLR','ADPUPA']:
@@ -95,16 +38,16 @@ while bufr.advance() == 0: # loop over messages.
             obdata = g.createVariable('obdata',np.float32,('nobs','obinfo'),zlib=True)
             oedata = g.createVariable('oberr',np.float32,('nobs','oeinfo'),zlib=True)
             qcdata = g.createVariable('qcinfo',np.float32,('nobs','qcinfo'),zlib=True)
-        for key in obstr_dict:
-            obdata.setncattr(key,obstr_dict[key])
+        for key in obstr.split():
+            obdata.setncattr(key,mnemonics_dict[key])
         obdata.missing_value=1.e11
         obdata.info = obstr
-        for key in oestr_dict:
-            oedata.setncattr(key,oestr_dict[key])
+        for key in oestr.split():
+            oedata.setncattr(key,mnemonics_dict[key])
         oedata.missing_value = 1.e11
         oedata.info = oestr
-        for key in qcstr_dict:
-            qcdata.setncattr(key,qcstr_dict[key])
+        for key in qcstr.split():
+            qcdata.setncattr(key,mnemonics_dict[key])
         qcdata.missing_value = 1.e11
         qcdata.info = qcstr
     while bufr.load_subset() == 0: # loop over subsets in message.
