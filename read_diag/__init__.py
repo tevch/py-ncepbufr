8395988334992535db8fcdad8425d95627ef16b3
Jeff Whitaker <jswhit@fastmail.fm>
2015-11-04 14:16:33 -0700

    add module for read gsi diag files

---
+++ read_diag/__init__.py
@@ -0,0 +1,26 @@
+import numpy as np
+import _read_convobs
+class diag_conv(object):
+    # read diag_conv file.
+    def __init__(self,filename):
+        nobs = _read_convobs.get_num_convobs(filename)
+        self.nobs = nobs; self.filename = filename
+    def read_obs(self):
+        h_x,x_obs,x_err,x_lon,x_lat,x_press,x_time,x_code,x_errorig,x_type =\
+        _read_convobs.get_convobs_data(self.filename, self.nobs)
+        obs_desc = []
+        for n in range(self.nobs):
+            s = x_type[n].tostring()
+            s = s.encode('ascii').replace('\x00','').strip()
+            obs_desc.append(s)
+        self.hx = h_x
+        self.obs = x_obs
+        self.oberr = x_err
+        self.lon = x_lon
+        self.lat = x_lat
+        self.press = x_press
+        self.time = x_time
+        self.code = x_code
+        self.oberr_orig = x_errorig
+        self.obtype = np.array(obs_desc,'S3')
+
