e89393b5ff46cbdbf6c0b9c6c0cc8c510826c4fd
Jeff Whitaker <jswhit@fastmail.fm>
2016-01-23 21:25:57 -0700

    update

--- utils/matchdiag.py
+++ utils/matchdiag.py
@@ -8,6 +8,9 @@ print 'read netcdf'
 nc_prepbufr = Dataset(ncfile)
 # observation id (station id/type code/lon/lat/time/elevation/pressure)
 obidstrs = nc_prepbufr['obid'][:]
+obidstrs2 = np.array([obid[:-6] for obid in obidstrs])
+#for id in obidstrs:
+#    if id.startswith('NZSP'): print id[:-6]
 print 'total number of diag obs = ',diag_conv.nobs
 print 'total number of prepbufr obs = ',nc_prepbufr.dimensions['nobs'].size 
 diag_conv.read_obs()
@@ -24,7 +27,10 @@ for nob in range(diag_conv.nobs):
     obtype = diag_conv.obtype[nob]
     obidstr = "%s %3i %6.2f %6.2f %6.2f %4i %6.1f" % \
     (stid,obcode,lon,lat,time,elev,press)
-    nobs_nc = np.nonzero(obidstrs == obidstr)[0]
+    if str(obcode).startswith('18') or str(obcode).startswith('28'):
+        nobs_nc = np.nonzero(obidstrs2 == obidstr[:-6])[0]
+    else:
+        nobs_nc = np.nonzero(obidstrs == obidstr)[0]
     print nob,obidstr,len(nobs_nc),'matches'
     if len(nobs_nc) == 0:
         count_nomatch += 1
