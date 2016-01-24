4f022113309bd864e3aba7824267db108cb6871f
Jeff Whitaker <jswhit@fastmail.fm>
2016-01-24 11:38:10 -0700

    don't include balloon drift  in obid string if it is outside assim window

--- utils/matchdiag.py
+++ utils/matchdiag.py
@@ -2,15 +2,17 @@ import read_diag
 from netCDF4 import Dataset
 import numpy as np
 obsfile = 'diag_conv_ges.2010102700'
-ncfile = 'prepbufr_2010102700.nc'
+ncfile = 'prepbufr_2015102700.nc'
 diag_conv = read_diag.diag_conv(obsfile,endian='big')
 print 'read netcdf'
 nc_prepbufr = Dataset(ncfile)
 # observation id (station id/type code/lon/lat/time/elevation/pressure)
 obidstrs = nc_prepbufr['obid'][:]
 obidl = obidstrs.tolist()
+#for obid in obidl:
+#    if obid.startswith('11120'): print obid
 print('%s non-unique ob ids' % str(len(obidl)-len(set(obidl))))
-# observation id not include pressure.
+# observation id not including pressure.
 obidstrs_nop = np.array([obid[:-6] for obid in obidstrs])
 print 'total number of diag obs = ',diag_conv.nobs
 print 'total number of prepbufr obs = ',nc_prepbufr.dimensions['nobs'].size 
@@ -26,10 +28,12 @@ for nob in range(diag_conv.nobs):
     elev = diag_conv.stnelev[nob]
     obcode = diag_conv.code[nob]
     obtype = diag_conv.obtype[nob]
-    obidstr = "%s %3i %6.2f %6.2f %6.2f %4i %6.1f" % \
+    obidstr = "%s %3i %6.2f %6.2f %9.5f %5i %6.1f" % \
     (stid,obcode,lon,lat,time,elev,press)
     nobs_nc = np.nonzero(obidstrs_nop == obidstr[:-6])[0]
     if len(nobs_nc) > 1: # if more than one match, include pressure
+        # skip if missing or invalid pressure
+        if press < 0 or press > 2.e4: continue
         nobs_nc = np.nonzero(obidstrs == obidstr)[0]
     print nob,obidstr,len(nobs_nc),'matches'
     if len(nobs_nc) == 0:
@@ -37,7 +41,18 @@ for nob in range(diag_conv.nobs):
         #raise ValueError('no match found')
     elif len(nobs_nc) > 1:
         count_multmatch += 1
-        #raise ValueError('multiple matches found')
+        hdrdat = nc_prepbufr['header'][nobs_nc]
+        qcdat = nc_prepbufr['qcdata'][nobs_nc]
+        obdat = nc_prepbufr['obdata'][nobs_nc]
+        print 'indices'
+        print nobs_nc
+        print 'headers'
+        print hdrdat
+        print 'qcinfo'
+        print qcdat
+        print 'obs'
+        print obdat
+        raise ValueError('multiple matches found')
 print('%s no matches' % count_nomatch)
 print('%s duplicate matches' % count_multmatch)
 nc_prepbufr.close()
