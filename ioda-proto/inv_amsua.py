fc07bca44a2a9c51336c57537e23b4ce95282e6b
Jeff Whitaker <jswhit@fastmail.fm>
2017-04-29 09:25:08 -0600

    update

--- ioda-proto/inv_amsua.py
+++ ioda-proto/inv_amsua.py
@@ -6,8 +6,8 @@ sat_id_dict_inv = {v: k for k, v in sat_id_dict.items()}
 h5_filename = sys.argv[1]
 f = tables.open_file(h5_filename,'r')
 table = f.root.amsua
-satids = table.col('sat_id').tolist()
-seen = set()
-uniq = [sat_id_dict_inv[x]+' ' for x in satids if x not in seen and not seen.add(x)]
-print('satellite ids in file: %s' % ''.join(uniq))
+satids = table.col('sat_id')
+satids_uniq = np.unique(satids)
+satids_uniq = [sat_id_dict_inv[s]+" " for s in satids_uniq]
+print('satellite ids in file: %s' % ''.join(satids_uniq))
 f.close()
